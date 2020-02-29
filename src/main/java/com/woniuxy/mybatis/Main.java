40d3b75c1ae3e44cb48352c692a604cecb510afa
DeYong_Zhou <350821905@qq.com>
2020-02-29 18:21:07 +0800

    insert 111

--- src/main/java/com/woniuxy/mybatis/Main.java
+++ src/main/java/com/woniuxy/mybatis/Main.java
@@ -12,6 +12,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 public class Main {
 	public static void main(String[] args) {
+		System.out.println("111");
 		SpringApplication.run(Main.class, args);
 	}
 
